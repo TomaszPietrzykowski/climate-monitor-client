c835c23e912bfee07a4760d67d286758e95034ce
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-08-19 13:50:25 +0200

    react intesection observer added, description animated

--- src/components/home/Description.js
+++ src/components/home/Description.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { useInView } from "react-intersection-observer";
+import { Spring, animated, config } from "react-spring/renderprops";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -35,34 +37,74 @@ const useStyles = makeStyles((theme) => ({
 
 const Description = () => {
   const classes = useStyles();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    trigger: 1,
+  });
+
   return (
     <div className={classes.container}>
       <div className={classes.tab}>
-        <div className={classes.title}>Up to date</div>
-        <div className={classes.text}>
-          Climate Monitor database is being updated with recently published data
-          of multiple origins, mainly NOAA and NASA public data servers.
-          Datasets update runs in relation to the data scope. Narrow scope data,
-          like daily readings and weekly mediana are not more then hours old.
-        </div>
+        <Spring
+          from={{ opacity: 0, marginTop: 50 }}
+          to={{ opacity: inView ? 1 : 0, marginTop: inView ? 0 : 50 }}
+          config={config.gentle}
+          delay={500}
+        >
+          {(props) => (
+            <animated.div style={props} ref={ref}>
+              <div className={classes.title}>Up to date</div>
+              <div className={classes.text}>
+                Climate Monitor database is being updated with recently
+                published data of multiple origins, mainly NOAA and NASA public
+                data servers. Datasets update runs in relation to the data
+                scope. Narrow scope data, like daily readings and weekly mediana
+                are not more then hours old.
+              </div>
+            </animated.div>
+          )}
+        </Spring>
       </div>
       <div className={classes.tab}>
-        <div className={classes.title}>Comprehensive</div>
-        <div className={classes.text}>
-          Well over 50 different datasets, collection of data on the most
-          important climate factors: levels of multiple atmospheric greenhouse
-          gases, land and ocean temperatures, trends in ocean levels as well as
-          condition of Arctica's and Antarctica's glaciers.
-        </div>
+        <Spring
+          from={{ opacity: 0, marginTop: 50 }}
+          to={{ opacity: inView ? 1 : 0, marginTop: inView ? 0 : 50 }}
+          config={config.gentle}
+          delay={500}
+        >
+          {(props) => (
+            <animated.div style={props} ref={ref}>
+              <div className={classes.title}>Comprehensivee</div>
+              <div className={classes.text}>
+                Well over 50 different datasets, collection of data on the most
+                important climate factors: levels of multiple atmospheric
+                greenhouse gases, land and ocean temperatures, trends in ocean
+                levels as well as condition of Arctica's and Antarctica's
+                glaciers.
+              </div>
+            </animated.div>
+          )}
+        </Spring>
       </div>
       <div className={classes.tab}>
-        <div className={classes.title}>Accessible</div>
-        <div className={classes.text}>
-          Data collected over multiple sources, delivered to you in one place,
-          served as an easy to read chart, as well as tabular or json data.
-          Analize scope of your choice and compare factors. Use free Public API
-          if you are up to something...
-        </div>
+        <Spring
+          from={{ opacity: 0, marginTop: 50 }}
+          to={{ opacity: inView ? 1 : 0, marginTop: inView ? 0 : 50 }}
+          config={config.gentle}
+          delay={500}
+        >
+          {(props) => (
+            <animated.div style={props} ref={ref}>
+              <div className={classes.title}>Accessible</div>
+              <div className={classes.text}>
+                Data collected over multiple sources, delivered to you in one
+                place, served as an easy to read chart, as well as tabular or
+                json data. Analize scope of your choice and compare factors. Use
+                free Public API if you are up to something...
+              </div>
+            </animated.div>
+          )}
+        </Spring>
       </div>
     </div>
   );
