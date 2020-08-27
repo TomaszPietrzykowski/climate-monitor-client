64da87c48fd454c7ad1001837806e2de3d971161
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-08-28 00:06:49 +0200

    data display wired

--- src/components/data/DataDisplay.js
+++ src/components/data/DataDisplay.js
@@ -1,21 +1,26 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Hidden from "@material-ui/core/Hidden";
 import { datasets } from "./Datasets";
+import BackIcon from "@material-ui/icons/ArrowBack";
+
+const drawerWidth = 350;
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-  },
+  //   container: {
+  //     display: "flex",
+  //     flexDirection: "column",
+  //   },
   root: {
     display: "flex",
+    maxWidth: 1200,
+    margin: "auto",
   },
   titleBar: {
     width: "100%",
@@ -33,31 +38,53 @@ const useStyles = makeStyles((theme) => ({
     margin: "0.8rem 4rem",
   },
   drawer: {
-    width: 300,
+    width: drawerWidth,
     zIndex: 1,
   },
   list: {
     marginTop: "10rem",
     marginBottom: "16rem",
-    width: 300,
-    flexShrink: 0,
+    width: drawerWidth,
+    // flexShrink: 0,
+  },
+  listItem: {
+    fontFamily: "Poppins",
+    fontWeight: "bold",
+    color: theme.palette.text.secondary,
+    fontSize: "1rem",
+    marginLeft: "1rem",
+    "&:hover": {
+      backgroundColor: theme.palette.grey.A100,
+    },
+  },
+  listItemActive: {
+    fontFamily: "Poppins",
+    fontWeight: "bold",
+    color: theme.palette.text.primary,
+    fontSize: "1rem",
+    borderLeft: "5px solid green",
+    marginLeft: "1rem",
+    "&:hover": {
+      cursor: "default",
+    },
   },
   drawerPaper: {
-    width: 300,
+    width: drawerWidth,
   },
   drawerContainer: {
     overflow: "auto",
     overflowX: "hidden",
   },
   content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
+    fontSize: "2rem",
+    padding: "3rem",
   },
 }));
 
 const DataDisplay = ({ dataset }) => {
   const activeDataset = datasets[dataset];
   const [activeData, setActiveData] = useState(activeDataset.scopes[0]);
+  const [index, setIndex] = useState(0);
   const classes = useStyles();
 
   const getData = async (endpoint) => {
@@ -68,6 +95,7 @@ const DataDisplay = ({ dataset }) => {
       const data = await res.json();
       console.log(data.data.title);
       setActiveData(data.data);
+      console.log(data.data);
     } catch (err) {
       console.log(err);
     }
@@ -78,8 +106,8 @@ const DataDisplay = ({ dataset }) => {
       <div className={classes.titleBar}>
         <div className={classes.sectionHeader}>{activeDataset.header}</div>
       </div>
-      <Fragment>
-        <div className={classes.root}>
+      <div className={classes.root}>
+        <Hidden mdDown>
           <Drawer
             className={classes.drawer}
             variant="permanent"
@@ -89,121 +117,102 @@ const DataDisplay = ({ dataset }) => {
           >
             <div className={classes.drawerContainer}>
               <List className={classes.list}>
-                {activeDataset.scopes.map((scope) => (
+                <ListItem
+                  className={classes.listItem}
+                  style={{ opacity: 0.7 }}
+                  button
+                  disableRipple
+                  key={"backbutton"}
+                  component={Link}
+                  to="/data"
+                >
+                  <BackIcon />
+                  Back to all data
+                </ListItem>
+                {activeDataset.scopes.map((scope, i) => (
                   <ListItem
+                    className={
+                      i === index ? classes.listItemActive : classes.listItem
+                    }
                     button
                     key={scope.title}
-                    onClick={() => getData(scope.endpoint)}
+                    onClick={() => {
+                      getData(scope.endpoint);
+                      setIndex(i);
+                    }}
                   >
-                    <ListItemText primary={scope.title} />
+                    {scope.title}
                   </ListItem>
                 ))}
                 <Divider />
               </List>
             </div>
           </Drawer>
-          <main className={classes.content}>
-            <Toolbar />
+        </Hidden>
+        <main className={classes.content}>
+          <Toolbar />
 
-            <h2>{activeData.title}</h2>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
-              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-              gravida rutrum quisque non tellus. Convallis convallis tellus id
-              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
-              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
-              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-              Metus vulputate eu sc elerisque felis imperdiet proin fermentum
-              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-              lobortis feugiat vivamus at augue. At augue eget arcu dictum
-              varius duis at consectetur lorem. Velit sed ullamcorper morbi
-              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
-              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-              gravida rutrum quisque non tellus. Convallis convallis tellus id
-              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
-              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
-              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-              Metus vulputate eu scelerisque felis imperdiet proin fermentum
-              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-              lobortis feugiat vivamus at augue. At augue eget arcu dictum
-              varius duis at consectetur lorem. Velit sed ullamcorper morbi
-              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
-              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-              gravida rutrum quisque non tellus. Convallis convallis tellus id
-              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
-              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
-              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-              Metus vulputate eu scelerisque felis imperdiet proin fermentum
-              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-              lobortis feugiat vivamus at augue. At augue eget arcu dictum
-              varius duis at consectetur lorem. Velit sed ullamcorper morbi
-              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
-              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-              gravida rutrum quisque non tellus. Convallis convallis tellus id
-              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
-              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
-              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-              Metus vulputate eu scelerisque felis imperdiet proin fermentum
-              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-              lobortis feugiat vivamus at augue. At augue eget arcu dictum
-              varius duis at consectetur lorem. Velit sed ullamcorper morbi
-              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
-              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-              gravida rutrum quisque non tellus. Convallis convallis tellus id
-              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
-              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
-              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-              Metus vulputate eu scelerisque felis imperdiet proin fermentum
-              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-              lobortis feugiat vivamus at augue. At augue eget arcu dictum
-              varius duis at consectetur lorem. Velit sed ullamcorper morbi
-              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-              ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-              elementum integer enim neque volutpat ac tincidunt. Ornare
-              suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-              volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-              Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-              ornare massa eget egestas purus viverra accumsan in. In hendrerit
-              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-              aliquam sem et tortor. Habitant morbi tristique senectus et.
-              Adipiscing elit duis tristique sollicitudin nibh sit. Ornare
-              aenean euismod elementum nisi quis eleifend. Commodo viverra
-              maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-              aliquam ultrices sagittis orci a.
-            </Typography>
-          </main>
-        </div>
-      </Fragment>
+          <h2>{activeData.title}</h2>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h5>{activeData.description}</h5>
+          <br />
+          <br />
+          <h5>{activeData.lastUpdate}</h5>
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+            dolor purus non enim praesent elementum facilisis leo vel. Risus at
+            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+            rutrum quisque non tellus. Convallis convallis tellus id interdum
+            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+            eu sc elerisque felis imperdiet proin fermentum leo. Mauris commodo
+            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+            vivamus at augue. At augue eget arcu dictum varius duis at
+            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+            dolor purus non enim praesent elementum facilisis leo vel. Risus at
+            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+            rutrum quisque non tellus. Convallis convallis tellus id interdum
+            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+            eu sc elerisque felis imperdiet proin fermentum leo. Mauris commodo
+            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+            vivamus at augue. At augue eget arcu dictum varius duis at
+            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+            dolor purus non enim praesent elementum facilisis leo vel. Risus at
+            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+            rutrum quisque non tellus. Convallis convallis tellus id interdum
+            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+            eu sc elerisque felis imperdiet proin fermentum leo. Mauris commodo
+            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+            vivamus at augue. At augue eget arcu dictum varius duis at
+            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.
+          </p>
+        </main>
+      </div>
     </div>
   );
 };
