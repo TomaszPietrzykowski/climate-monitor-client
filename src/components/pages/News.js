c5e9ecdf50c7183fd2c3fcc070a92c2a91b9fef2
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-05-09 20:55:38 +0200

    pagination added to news

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,28 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
+import Pagination from "@material-ui/lab/Pagination"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  paginationRoot: {
+    margin: "3rem",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}))
 
 const News = () => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([])
+  const [page, setPage] = useState(1)
+  const [pages, setPages] = useState(1)
+  const [loading, setLoading] = useState(false)
+
+  const classes = useStyles()
+
   const getNews = async () => {
-    setLoading(true);
+    setLoading(true)
     try {
-      const res = await fetch(`http://localhost:5000/api/news`);
-      const data = await res.json();
-      setArticles(data.articles);
-      setLoading(false);
+      const res = await fetch(`http://localhost:5000/api/news?page=${page}`)
+      const data = await res.json()
+      setArticles(data.articles)
+      setPages(data.pages)
+      setLoading(false)
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
+
+  const handleChange = (event, value) => {
+    setPage(value)
+  }
 
   useEffect(() => {
-    getNews();
+    getNews(page)
     // eslint-disable-next-line
-  }, []);
+  }, [page])
 
   return (
     <div style={{ maxWidth: 1400, margin: "auto" }}>
       <h1>News1</h1>
+      {!loading && pages > 1 && (
+        <div className={classes.paginationRoot}>
+          <Pagination count={pages} page={page} onChange={handleChange} />
+        </div>
+      )}
       {loading ? (
         <h2>loading</h2>
       ) : (
@@ -42,8 +68,13 @@ const News = () => {
           </div>
         ))
       )}
+      {!loading && pages > 1 && (
+        <div className={classes.root}>
+          <Pagination count={pages} page={page} onChange={handleChange} />
+        </div>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default News;
+export default News
